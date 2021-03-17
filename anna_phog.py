034c6b72791b58ace97f83907357167dde3cfc64
alexandre durrmeyer <alexandre.durmeyer@edu.esiee.fr>
2021-03-17 10:24:08 +0100

    corrected level error

--- anna_phog.py
+++ anna_phog.py
@@ -7,18 +7,18 @@ import matplotlib.pyplot as plt
 original matlab code at https://www.robots.ox.ac.uk/~vgg/research/caltech/phog.html
 '''
 
-def anna_phog(Img, bin, angle, L, roi, save=False):
+def anna_phog(Img, bin, angle, L, roi):
     '''
      anna_PHOG Computes Pyramid Histogram of Oriented Gradient over a ROI.
-                   
+
      [BH, BV] = anna_PHOG(Img,BIN,ANGLE,L,ROI) computes phog descriptor over a ROI.
-     
+
      Given and image Img, phog computes the Pyramid Histogram of Oriented Gradients
      over L pyramid levels and over a Region Of Interest
 
     IN:
         Img- Images numpy array of size MxN (Color or Gray)
-        bin - Number of bins on the histogram 
+        bin - Number of bins on the histogram
         angle - 180 or 360
        L - number of pyramid levels
        roi - Region Of Interest (ytop,ybottom,xleft,xright)
@@ -27,10 +27,10 @@ def anna_phog(Img, bin, angle, L, roi, save=False):
         p - pyramid histogram of oriented gradients
     '''
     if Img.shape[2] == 3:
-        G = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY) 
+        G = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
     else:
         G = Img
-        
+
 
     if np.sum(G) > 100:
         # apply automatic Canny edge detection using the computed median
@@ -43,24 +43,24 @@ def anna_phog(Img, bin, angle, L, roi, save=False):
         plt.show()
         GradientX, GradientY = np.gradient(G)
         GradientYY = np.gradient(GradientY, axis=1)
-        
+
         Gr = np.sqrt(np.square(GradientX)+np.square(GradientY))
         index = GradientX == 0
         GradientX[index] = 1e-5 #maybe another value
-                
+
         YX = GradientY*GradientX
         if angle == 180: A = ((np.arctan(YX)+(np.pi/2))*180)/np.pi
         if angle == 360: A = ((np.arctan2(GradientY,GradientX)+np.pi)*180)/np.pi
-                                    
+
         bh, bv = anna_BinMatrix(A,E,Gr,angle,bin)
-        
+
     else:
         bh = np.zeros(Img.shape)
         bv = np.zeros(Img.shape)
 
     bh_roi = bh[roi[0]:roi[1], roi[2]:roi[3]]
     bv_roi = bv[roi[0]:roi[1], roi[2]:roi[3]]
-    
+
     p = anna_PhogDescriptor(bh_roi,bv_roi,L,bin)
     return p
 
@@ -71,14 +71,14 @@ def anna_BinMatrix(A,E,G,angle,bin):
     (i,j) position contains the histogram value for the pixel at position (i,j)
     and another matrix (bv) where the position (i,j) contains the gradient
     value for the pixel at position (i,j)
-                   
+
     IN:
     	A - Matrix containing the angle values
     	E - Edge Image
         G - Matrix containing the gradient values
     	angle - 180 or 360
-        bin - Number of bins on the histogram 
-    
+        bin - Number of bins on the histogram
+
     OUT:
     	bm - matrix with the histogram values
         bv - matrix with the gradient values (only for the pixels belonging to and edge)
@@ -93,28 +93,28 @@ def anna_BinMatrix(A,E,G,angle,bin):
     nAngle = angle/bin
 
     for i in range(n):
-        posY, posX = np.where(contorns==i) 
+        posY, posX = np.where(contorns==i)
         for j in range(posY.shape[0]):
             pos_x = posX[j]
             pos_y = posY[j]
-            
+
             b = np.ceil(A[pos_y,pos_x]/nAngle)
             if b==0: bin=1
             if G[pos_y,pos_x]>0:
                 bm[pos_y,pos_x] = b
                 bv[pos_y,pos_x] = G[pos_y,pos_x]
-                
+
     return (bm, bv)
-    
-    
+
+
 
 def anna_PhogDescriptor(bh,bv,L,bin):
     '''
      anna_PHOGDESCRIPTOR Computes Pyramid Histogram of Oriented Gradient over a ROI.
-                   
+
     IN:
         bh - matrix of bin histogram values
-        bv - matrix of gradient values 
+        bv - matrix of gradient values
        L - number of pyramid levels
        bin - number of bins
 
@@ -122,27 +122,25 @@ def anna_PhogDescriptor(bh,bv,L,bin):
         p - pyramid histogram of oriented gradients (phog descriptor)
     '''
     p = []
-    cella = 1
+    #level 0
+    for b in range(bin):
+        ind = bh==b
+        p = np.sum(bv[ind])
+
+    #higher levels
     for l in range(L):
         x = int(np.trunc(bh.shape[1]/(2**l)))
         y = int(np.trunc(bh.shape[0]/(2**l)))
-        xx=0
-        yy=0
         for xx in range(0, bh.shape[1]-x, x):
             for yy in range(0, bh.shape[0]-y, y):
                 bh_cella = bh[yy:yy+y, xx:xx+x]
                 bv_cella = bv[yy:yy+y, xx:xx+x]
-                
+
                 for b in range(bin):
                     ind = bh_cella==b
                     p = np.append(p, np.sum(bv_cella[ind], axis=0))
-                yy += y
-                    
-            cella += 1
-            yy = 0
-            xx += x
-        
+
     if np.sum(p)!=0:
         p = p/np.sum(p)
-        
+
     return p
--- anna_phog_demo.py
+++ anna_phog_demo.py
@@ -11,6 +11,6 @@ roi = [1,225,1,300]
 save=True
 
 Image = imageio.imread(image_path)
-p = anna_phog(Image, bin, angle, Level, roi, save)
+p = anna_phog(Image, bin, angle, Level, roi)
 print("P: \n{}".format(p))
 print(len(p), type(p))
